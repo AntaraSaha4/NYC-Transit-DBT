3df94b21d267beee6bb87e90540bc6194562c32b
Antara Saha <antarasa@andrew.cmu.edu>
2023-07-23 10:15:46 -0400

    Removed nyc_transit due to corrupt files

--- Assignments/nyc_transit/models/mart/mart__dim_locations.sql
+++
@@ -1,3 +0,0 @@
-SELECT
-{{dbt_utils.star(ref('taxi+_zone_lookup')) }}
-FROM {{ ref('taxi+_zone_lookup')}}
--- Assignments/nyc_transit/models/mart/mart__fact_all_trips_daily.sql
+++
@@ -1,7 +0,0 @@
-SELECT
-    type,
-    date_trunc('day', started_at_ts)::date as date,
-    count(*) as trips,
-    avg(duration_min) as average_trip_duration_min
-FROM {{ ref('mart__fact_all_trips')}}
-GROUP BY ALL
